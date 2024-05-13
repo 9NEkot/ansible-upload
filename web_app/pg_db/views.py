5c21d9741fb13280d20ee90901f9cbf3857f7522
9NEkot <neon7311@gmail.com>
2024-05-13 10:39:15 -0400

    pre-pre

--- web_app/pg_db/views.py
+++ web_app/pg_db/views.py
@@ -1,7 +1,8 @@
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
+import subprocess as sp
 
 
 @api_view()
 def healthcheck(request):
-    return Response({"message": "Hello, world!"})
+    return Response({f"message": f'{sp.run(["cat", "/opt/qwe"])}'})
