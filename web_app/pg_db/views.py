558072915c88a65889fb20b4dcbc6bfe50c64c0d
9NEkot <neon7311@gmail.com>
2024-05-15 09:07:51 -0400

    status

--- web_app/pg_db/views.py
+++ web_app/pg_db/views.py
@@ -5,4 +5,14 @@ import subprocess as sp
 
 @api_view()
 def healthcheck(request):
-    return Response({f"message": f'{sp.run(["cat", "/opt/qwe"])}'})
+    message = {}
+
+    runn = ["web_app", "pg_app", "webapp_nginx"]
+    "docker inspect web_app |jq '.[].State'"
+    for r in runn:
+
+        sp.run(["echo", r, ">", "/docker-status-pipe"])
+        with open("/out.sh", "r") as t:
+            message.update({r: t.read()})
+
+    return Response(message)
