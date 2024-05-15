5ea69ce0f3fbd1674ed391f411772a5f8b72a596
9NEkot <neon7311@gmail.com>
2024-05-15 12:11:47 -0400

    envfilew

--- web_app/pg_db/views.py
+++ web_app/pg_db/views.py
@@ -1,6 +1,5 @@
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
-import subprocess as sp
 import os
 
 
@@ -9,7 +8,7 @@ def healthcheck(request):
     message = {}
 
     runn = ["web_app", "pg_app", "webapp_nginx"]
-    "docker inspect web_app |jq '.[].State'"
+    # "docker inspect web_app |jq '.[].State'"
     for r in runn:
         rr = f"docker inspect {r} |jq '.[].State'"
 
@@ -17,4 +16,5 @@ def healthcheck(request):
         
         with open("/out.sh", "r") as t:
             message.update({r: t.read()})
+
     return Response({f"message": message})
