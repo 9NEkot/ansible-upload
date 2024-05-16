99b7bc36e8995015ebee7161f45df7985e471d4c
9NEkot <neon7311@gmail.com>
2024-05-16 02:55:14 -0400

    envfilew

--- web_app/pg_db/views.py
+++ web_app/pg_db/views.py
@@ -16,7 +16,7 @@ def healthcheck(request):
         
 
 
-        with open("/out.sh", "r") as t:
+        with open("/docker-out.txt", "r") as t:
             message.update({r: t.read()})
 
     return Response({f"message": message})
