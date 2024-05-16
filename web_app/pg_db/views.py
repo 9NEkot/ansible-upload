c7c334b8c31415479c620bd35c6bcfa64c007ee0
9NEkot <neon7311@gmail.com>
2024-05-16 03:49:32 -0400

    fiks

--- web_app/pg_db/views.py
+++ web_app/pg_db/views.py
@@ -8,7 +8,7 @@ def healthcheck(request):
     message = {}
 
     runn = ["web_app", "pg_app", "webapp_nginx"]
-    os.system(f"echo web_app > /docker-status-pipe")
+    # os.system(f"echo web_app > /docker-status-pipe")
     # "docker inspect web_app |jq '.[].State'"
     # for r in runn:
     #     # rr = f"docker inspect {r} |jq '.[].State'"
@@ -16,6 +16,8 @@ def healthcheck(request):
 
     #     # os.system(f"echo {rr} > /docker-status-pipe ")
     #     os.system(f"echo {rr} > /docker-status-pipe")
+    with open("/docker-status-pipe", "w") as pipe:
+        pipe.write("doker inspect web_app")
 
     with open("/out.sh", "r") as t:
         message.update({"web+app": t.read()})
